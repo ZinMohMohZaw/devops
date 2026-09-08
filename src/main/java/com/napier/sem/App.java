22bc33a0b60ed81310f96c0b54fe524fb4752a79
ZinMohMohZaw <40870399@live.napier.ac.uk>
2026-09-08 17:17:20 +0630

    Added Horizon Gen Lock Dialog Lines

---
+++ src/main/java/com/napier/sem/App.java
@@ -0,0 +1,9 @@
+package com.napier.sem;
+
+public class App {
+    public static void  main (String[] args)
+    {
+        System.out.println("Hold for Identiscan... [Scanner light pulses]");
+        System.out.println("Alpha Registry restored. Genetic profile confirmed: Dr. Elisabet Sobeck. Entry authorized.");
+    }
+}
